af52f45a7e257201f996c9ae2a57c0ea112b29df
sean <sbelomet@student.42lausanne.ch>
2025-09-24 19:57:17 +0200

    ex07 done

--- 00-starting/ex07/sos.py
+++ 00-starting/ex07/sos.py
@@ -1,51 +1,56 @@
 import sys
 
 
-NESTED_MORSE = {" ": "/",
-                "A": ".-",
-                "B": "-...",
-                "C": "-.-.",
-                "D": "-..",
-                "E": ".",
-                "F": "..-.",
-                "G": "--.",
-                "H": "....",
-                "I": "..",
-                "J": ".---",
-                "K": "-.-",
-                "L": ".-..",
-                "M": "--",
-                "N": "-.",
-                "O": "---",
-                "P": ".--.",
-                "Q": "--.-",
-                "R": ".-.",
-                "S": "...",
-                "T": "-",
-                "U": "..-",
-                "V": "...-",
-                "W": ".--",
-                "X": "-..-",
-                "Y": "-.--",
-                "Z": "--..",
-                "1": ".----",
-                "2": "..---",
-                "3": "...--",
-                "4": "....-",
-                "5": ".....",
-                "6": "-....",
-                "7": "--...",
-                "8": "---..",
-                "9": "----.",
-                "0": "-----"}
+def time_to_go_morse_mode(string):
+    NESTED_MORSE = {" ": "/",
+                    "A": ".-",
+                    "B": "-...",
+                    "C": "-.-.",
+                    "D": "-..",
+                    "E": ".",
+                    "F": "..-.",
+                    "G": "--.",
+                    "H": "....",
+                    "I": "..",
+                    "J": ".---",
+                    "K": "-.-",
+                    "L": ".-..",
+                    "M": "--",
+                    "N": "-.",
+                    "O": "---",
+                    "P": ".--.",
+                    "Q": "--.-",
+                    "R": ".-.",
+                    "S": "...",
+                    "T": "-",
+                    "U": "..-",
+                    "V": "...-",
+                    "W": ".--",
+                    "X": "-..-",
+                    "Y": "-.--",
+                    "Z": "--..",
+                    "1": ".----",
+                    "2": "..---",
+                    "3": "...--",
+                    "4": "....-",
+                    "5": ".....",
+                    "6": "-....",
+                    "7": "--...",
+                    "8": "---..",
+                    "9": "----.",
+                    "0": "-----"}
+    uppered = string.upper()
+    morse = [NESTED_MORSE[c] for c in uppered]
+    print(' '.join(morse))
+
 
 def main():
     try:
         if len(sys.argv) != 2:
-            raise AssertionError("One arguments expected (string and integer)")
-        if not sys.argv[2].isalnum():
-            raise AssertionError("Two arguments expected (string and integer)")
-        string = sys.argv[1]
+            raise AssertionError("One arguments expected (string)")
+        if not all(c.isalnum() or c == ' ' for c in sys.argv[1]):
+            raise AssertionError("Only letters, numbers and spaces allowed")
+        time_to_go_morse_mode(sys.argv[1])
     except AssertionError as e:
         print("AssertionError:", e)
 
